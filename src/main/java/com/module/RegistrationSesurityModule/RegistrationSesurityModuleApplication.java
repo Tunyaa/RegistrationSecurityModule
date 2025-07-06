48553198dc85d6f6ea001982422484cc549ef59e
tunyaa <ildcampthunder@gmail.com>
2025-07-06 17:19:56 +0300

    init

---
+++ src/main/java/com/module/RegistrationSesurityModule/RegistrationSesurityModuleApplication.java
@@ -0,0 +1,13 @@
+package com.module.RegistrationSesurityModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationSesurityModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationSesurityModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/module/RegistrationSesurityModule/RegistrationSesurityModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.RegistrationSesurityModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationSesurityModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
